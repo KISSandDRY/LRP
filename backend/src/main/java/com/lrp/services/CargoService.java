a2a3eefe7f4e600d5461a9fe0a32733f62ed5fa1
Roman <roman.samets@gmail.com>
2026-04-18 21:38:02 +0300

    [chore] add logging

--- backend/src/main/java/com/lrp/services/CargoService.java
+++ backend/src/main/java/com/lrp/services/CargoService.java
@@ -2,12 +2,15 @@ package com.lrp.services;
 
 import com.lrp.models.Cargo;
 import com.lrp.repositories.CargoRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class CargoService {
+    private static final Logger logger = LoggerFactory.getLogger(CargoService.class);
     private final CargoRepository cargoRepository;
 
     public CargoService(CargoRepository cargoRepository) {
@@ -15,10 +18,12 @@ public class CargoService {
     }
 
     public List<Cargo> getAllCargo() {
+        logger.debug("Fetching all cargo from the database.");
         return cargoRepository.findAll();
     }
 
     public Cargo addCargo(Cargo cargo) {
+        logger.info("Adding tracking for new cargo order of type logs: {}", cargo.getClass().getSimpleName());
         return cargoRepository.save(cargo);
     }
 }
--- backend/src/main/java/com/lrp/services/VehicleService.java
+++ backend/src/main/java/com/lrp/services/VehicleService.java
@@ -2,12 +2,15 @@ package com.lrp.services;
 
 import com.lrp.models.Vehicle;
 import com.lrp.repositories.VehicleRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class VehicleService {
+    private static final Logger logger = LoggerFactory.getLogger(VehicleService.class);
     private final VehicleRepository vehicleRepository;
 
     public VehicleService(VehicleRepository vehicleRepository) {
@@ -15,10 +18,12 @@ public class VehicleService {
     }
 
     public List<Vehicle> getAllVehicles() {
+        logger.debug("Fetching all vehicles from the database.");
         return vehicleRepository.findAll();
     }
 
     public Vehicle addVehicle(Vehicle vehicle) {
+        logger.info("Adding new vehicle of type: {}", vehicle.getClass().getSimpleName());
         return vehicleRepository.save(vehicle);
     }
 }
